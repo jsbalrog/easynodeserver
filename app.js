1e00ce31bacb910ef86df5763ea1286b1f3d3316
Ted Jenkins <buffalobillion@gmail.com>
2013-05-02 12:58:14 -0600

    more commits

---
+++ app.js
@@ -0,0 +1,27 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+app.configure(function(){
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Start server
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
